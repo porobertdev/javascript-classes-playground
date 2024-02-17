c7e3e2a5b51d7620216d6dcdd59af5df6da8de45
Robert P. <porobertdev@gmail.com>
2024-02-17 21:44:54 +0200

    Create `webpack.config.js`
    * Used to automate the build process
    * Just ./node_modules/.bin/webpack is needed now

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    mode: 'development',
+    entry: './article.js',
+    output: {
+        filename: 'main.js',
+        publicPath: 'dist'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
